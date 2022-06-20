1bd3752cc3ee7923e742e1cceceb363a14945fcf
EkaterinaMikhalko <mikhalkoea@gmail.com>
2022-06-21 01:11:38 +0300

    Task 1

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV1 {
+
     @Test
+
     void shouldReturnDemoAccounts() {
       // Given - When - Then
       // Предусловия
